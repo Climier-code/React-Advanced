2831865f693bb1ff66d6e7062d9b1deb31eb285a
Climier-code <ikowhddus@naver.com>
2021-04-28 22:08:49 +0900

    Feat Blog-FrontEnd: Can Update and Remove Post

--- Blog/blog-frontend/src/index.js
+++ Blog/blog-frontend/src/index.js
@@ -10,6 +10,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
@@ -31,9 +32,11 @@ loadUser();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <HelmetProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
--- Blog/blog-frontend/src/pages/WritePage.js
+++ Blog/blog-frontend/src/pages/WritePage.js
@@ -3,10 +3,14 @@ import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
 import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 import Responsive from '../components/common/Responsive';
+import { Helmet } from 'react-helmet-async';
 
 const WritePage = () => {
   return (
     <Responsive>
+      <Helmet>
+        <title>새 글 작성하기 - Write Your Life</title>
+      </Helmet>
       <EditorContainer />
       <TagBoxContainer />
       <WriteActionButtonsContainer />
