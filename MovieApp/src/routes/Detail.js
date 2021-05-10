373c5fd8d333e5b780405f847a8f3025c9a2b622
Climier-code <ikowhddus@naver.com>
2021-05-11 00:24:16 +0900

    Finish movie-app: Can see movie-detail page

--- MovieApp/src/routes/Detail.js
+++ MovieApp/src/routes/Detail.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import './Detail.css';
 
-function Detail( {location, history} ) {
+function Detail({ location, history }) {
+
+    const {id, year, title, summary, poster, genres} = location.state;
+    
     if (location.state === undefined) {
         history.push("/");
     }
-    console.log(location);
     return (
-        location.state && <span>{location.state.title}</span>
+        location.state &&
+        <div className="movie-detail">
+            <div className="movie-detailTop">
+                <div className="movie2">
+                    <h2 className="movie-title">{title}</h2>
+                    <div className="movie3">
+                        <ul className="movie-genres">
+                            {genres.map((genre, index) => <li key={index} className="genres-genre">{genre}</li>)}
+                        </ul>
+                        <h5 className="movie-year"> {year} </h5>
+                    </div>
+                </div>
+                <img src={poster} alt={title} title={title} />
+            </div>
+            <p className="movie-summary">{summary} </p>
+        </div>
     );
 }
 
